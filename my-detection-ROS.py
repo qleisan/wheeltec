f167d8bd33aa13c4a73f58ecae421c45779f82b8
wheeltec <wheeltec@wheeltec.semcon.se>
2022-01-29 09:52:54 -0500

    Remove use of CV bridge from my-detection-ROS.py

--- my-detection-ROS.py
+++ my-detection-ROS.py
@@ -11,37 +11,40 @@
 import rospy
 from sensor_msgs.msg import Image
 
-from cv_bridge import CvBridge, CvBridgeError
-import cv2
-
 import jetson.inference
 import jetson.utils
 from time import sleep
 
+import numpy as np
 
 net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
 
-
-# Instantiate CvBridge
-bridge = CvBridge()
-
 def image_callback(msg):
     print("Received an image!")
-    try:
-        # Convert your ROS Image message to OpenCV2
-        cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-    except CvBridgeError, e:
-        print(e)
-    else:
-        # Save your OpenCV2 image as a jpeg 
-        cv2.imwrite('camera_image_X.jpeg', cv2_img)
-        #img = jetson.utils.loadImage("camera_image_X.jpeg")
-        # TODO: for now load another image with interesting content
-        img = jetson.utils.loadImage("peds_0.jpg")
-        #img = jetson.utils.loadImage("my-recognition-python/polar_bear.jpg")
-        detections = net.Detect(img)
-        for detection in detections:
-            print(net.GetClassDesc(detection.ClassID))
+    print(msg.encoding, msg.width, msg.height)
+    print(type(msg.data))
+
+    im = np.frombuffer(msg.data, dtype=np.uint8).reshape(msg.height, msg.width, -1)
+    print(im.shape)
+
+    cuda_img = jetson.utils.cudaAllocMapped(width=640, height=480, format='rgb8')
+    cepa = jetson.utils.cudaToNumpy(cuda_img)
+
+    for y in range(0,msg.height):
+        for x in range(0,msg.width):
+            cepa[y,x,0] = im[y,x,0]
+            cepa[y,x,1] = im[y,x,1]
+            cepa[y,x,2] = im[y,x,2]
+
+    jetson.utils.saveImage("cuda_test.jpg", cuda_img)
+    print("--------------")
+    # TODO: for now load another image with interesting content
+    img = jetson.utils.loadImage("peds_0.jpg")
+    #img = jetson.utils.loadImage("my-recognition-python/polar_bear.jpg")
+    #detections = net.Detect(img)
+    detections = net.Detect(cuda_img)
+    for detection in detections:
+        print(net.GetClassDesc(detection.ClassID))
 
 def main():
     rospy.init_node('image_listener')
